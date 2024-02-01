8f429a5c2e7582cbd30a6993a393d6ea3f93a327
KaiKimera <mail@kai.kim>
2024-02-01 04:31:47 +0000

    2024-02-01 04:31:47

--- MW_EXT_Tooltip.i18n.magic.php
+++ MW_EXT_Tooltip.i18n.magic.php
@@ -6,14 +6,14 @@
  * @file
  * @ingroup Extensions
  * @license MIT
- * @author iHub TO <https://ihub.to>
+ * @author Kai Kimera <https://kai.kim>
  */
 $magicWords = [];
 
 /**
  * English.
  *
- * @author iHub TO <https://ihub.to>
+ * @author Kai Kimera <https://kai.kim>
  */
 $magicWords['en'] = [
   'tooltip' => [0, 'tooltip'],
@@ -22,7 +22,7 @@ $magicWords['en'] = [
 /**
  * Russian.
  *
- * @author iHub TO <https://ihub.to>
+ * @author Kai Kimera <https://kai.kim>
  */
 $magicWords['ru'] = [
   'tooltip' => [0, 'tooltip'],
